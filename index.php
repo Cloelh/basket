fe5dbf269b8c776d210ccf0e7aca88b9a5b68f89
Cloelh <cloelh@outlook.fr>
2020-01-08 11:40:05 +0100

    commit

--- index.php
+++ index.php
@@ -1,38 +1,6 @@
 <?php get_header(); ?>
 
 
-
-<form method="POST" action="">
-	<select name="journee">
-		<option>1</option>
-		<option>2</option>
-		<option>3</option>
-		<option>4</option>
-		<option>5</option>
-		<option>6</option>
-		<option>7</option>
-		<option>8</option>
-		<option>9</option>
-		<option>10</option>
-		<option>11</option>
-	</select>
-	<input type="submit" name="jouneechoisit">
-</form>
-
-<?php
-	if (isset($_POST['jouneechoisit'])) {
-		$journeechoisit=$_POST['journee'];
-		calendrierchoisit($journeechoisit);
-	}
-	else {
-		
-		calendrier();
-	}
-?>
-
-
-
-
 <h2>Classement</h2>
 <?php classement(); ?>
 
---
+++ pages/calendrier.php
@@ -0,0 +1,27 @@
+<form method="POST" action="">
+	<select name="journee">
+		<option>1</option>
+		<option>2</option>
+		<option>3</option>
+		<option>4</option>
+		<option>5</option>
+		<option>6</option>
+		<option>7</option>
+		<option>8</option>
+		<option>9</option>
+		<option>10</option>
+		<option>11</option>
+	</select>
+	<input type="submit" name="jouneechoisit">
+</form>
+
+<?php
+	if (isset($_POST['jouneechoisit'])) {
+		$journeechoisit=$_POST['journee'];
+		calendrierchoisit($journeechoisit);
+	}
+	else {
+		
+		calendrier();
+	}
+?>
